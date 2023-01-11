647b98315576becee4b29aec0824f6f71b49c3e3
srdjanlazarevic11 <101801356+srdjanlazarevic11@users.noreply.github.com>
2023-01-11 17:09:03 +0100

    Basic Service-Client connection implemented

---
+++ sbes/Client/ClientProxy.cs
@@ -0,0 +1,42 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientProxy : ChannelFactory<IServiceManager>, IServiceManager, IDisposable
+    {
+        IServiceManager factory;
+
+        public ClientProxy(Binding binding, string remoteAddress) : base(binding, remoteAddress)
+        {
+            factory = this.CreateChannel();
+        }
+
+        public bool Connect(byte[] encryptedSessionKey)
+        {
+            bool connected = false;
+            try
+            {
+                connected = factory.Connect(encryptedSessionKey);
+
+            }
+            catch (FaultException<SecurityException> sec)
+            {
+                Console.WriteLine(sec.Message);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return connected;
+        }
+
+    }
+}
---
+++ sbes/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using Interfaces;
+using ServiceManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:8888/WCFService";
+
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            Console.WriteLine("Korisnik koji je pokrenuo klijenta je : " + WindowsIdentity.GetCurrent().Name);
+
+            EndpointAddress endpointAddress = new EndpointAddress(new Uri(address), EndpointIdentity.CreateUpnIdentity("wcfServer"));
+
+            Console.ReadLine();
+        }
+    }
+}
